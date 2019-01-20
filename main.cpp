74273d2532cde5ae7a9526cc6dbdf07f6a06906b
sjli <2016226040006@stu.scu.edu.cn>
2019-01-20 23:09:02 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,264 +1,134 @@
-#include "PassImageProc.h"
-#include <string> 
-#include <io.h>
-#include <process.h>
-#include <ctype.h>
+#include <opencv2/highgui/highgui.hpp>    
+#include <opencv2/imgproc/imgproc.hpp>
+#include "OCRRecognize.h"
+#include <iostream>
 #include <time.h>
 #include <fstream>
 
 using namespace cv;
-using namespace std;
 
-#define CAM
-//#define PIC
-
-bool bInit = true;
-bool bStopRecognize = false;
-bool bContinueProc = true;
-std::vector<cv::Mat> g_ResultImgs;
-std::vector<std::string> g_PassNumberArray;
-std::vector<std::string> g_PassNameArray;
-std::vector<std::string> g_TextArray;
-
-std::vector<cv::Vec4i> g_lines;
-
-clock_t start, finish;
-double  duration;
-
-///调整图片分辨率
-bool SetResolution(const char* path, int iResolution) {
-	FILE * file = fopen(path, "rb+");// - 打开图片文件 	
-	if (!file)return false;
-	int len = _filelength(_fileno(file));// - 获取文件大小
-	char* buf = new char[len];
-	fread(buf, sizeof(char), len, file);// - 将图片数据读入缓存 	
-	char * pResolution = (char*)&iResolution;// - iResolution为要设置的分辨率的数值，如72dpi 	
-											 // - 设置JPG图片的分辨率 	
-	buf[0x0D] = 1;// - 设置使用图片密度单位 	
-				  // - 水平密度，水平分辨率 	
-	buf[0x0E] = pResolution[1];
-	buf[0x0F] = pResolution[0]; 	// - 垂直密度，垂直分辨率 	
-	buf[0x10] = pResolution[1];
-	buf[0x11] = pResolution[0];  	// - 将文件指针移动回到文件开头 	
-	fseek(file, 0, SEEK_SET); 	// - 将数据写入文件，覆盖原始的数据，让修改生效 	
-	fwrite(buf, sizeof(char), len, file);
-	fclose(file);
-	return true;
-}
-
-///验证护照编码
-byte Passport_checkSumSubString(byte *src, int len)
+int main(int argc, char *argv[])
 {
-	int i, chk = 0;
-	byte wmap[] = { 10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35 };
-	byte xmap[] = { 0,1,2,3,4,5,6,7,8,9 };
-	byte cmap[] = { 7,3,1 };
-	int cmapindex = 0;
-	for (i = 0; i<len; i++)
+	OCRRecognize rec;
+
+	//std::ofstream f("rec_res.txt");
+	//if (!f)
+	//{
+	//	return -1;
+	//}
+
+	for (int i = 1; i <= 6; i++)
 	{
-		if ((src[i] >= 'A') && (src[i] <= 'Z'))
+		//f << "i: " << i << std::endl;
+		cv::Mat reImg = imread("0" + std::to_string(i)+".jpg", 0);
+		if (reImg.empty())
 		{
-			chk += wmap[src[i] - 'A'] * cmap[cmapindex++];
+			return -1;
 		}
-		else if ((src[i] >= '0') && (src[i] <= '9'))
+		char *pOutSrc = rec.recognize(reImg);
+		std::cout << "pOutSrc: " << pOutSrc << std::endl;
+		//f << "pOutSrc: " << pOutSrc;
+		cv::imshow("src", reImg);
+
+		////直方图均衡化
+		//cv::Mat reImgCopy1;
+		//reImg.copyTo(reImgCopy1);
+		//cv::Rect rect1(0, 0, reImg.cols / 1, reImg.rows);
+		//cv::equalizeHist(reImgCopy1(rect1), reImgCopy1(rect1));
+		//cv::imshow("reImgCopy1", reImgCopy1);
+		//char *pOut1 = rec.recognize(reImgCopy1);
+		//std::cout << "pOut1: " << pOut1 << std::endl;
+		
+		////线性变化
+		//cv::Mat reImgCopy2 = cv::Mat::zeros(reImg.size(), reImg.type());
+		//reImg.copyTo(reImgCopy2);
+		//cv::convertScaleAbs(reImgCopy2, reImgCopy2, 0.8, -20);
+		//cv::normalize(reImgCopy2, reImgCopy2, 0, 255, NORM_MINMAX);
+		//char *pOut2 = rec.recognize(reImgCopy2);
+		//std::cout << "pOut2: " << pOut2 << std::endl;
+		////f << "pOut2: " << pOut2;
+		//cv::imshow("reImgCopy2", reImgCopy2);
+		////cv::waitKey(0);
+		
+		//拉普拉斯增强
+		cv::Mat reImgCopy3;
+		reImg.copyTo(reImgCopy3);
+		cv::Rect rect(0, 0, reImgCopy3.cols / 1, reImgCopy3.rows);
+
+		cv::Mat kernel = (cv::Mat_<float>(3, 3) << 0, -1, 0, 0, 5, 0, 0, -1, 0);
+		cv::filter2D(reImg(rect), reImgCopy3(rect), CV_8UC1, kernel);
+
+		char *pOut3 = rec.recognize(reImgCopy3);
+
+		std::cout << "pOut3: " << pOut3 << std::endl;
+		//f << "pOut3: " << pOut3;
+		cv::imshow("reImgCopy3", reImgCopy3);
+
+		//log处理
+		cv::Mat reImgCopy4;
+		reImg.copyTo(reImgCopy4);
+		for (int i = 0; i < reImgCopy4.rows; i++)
 		{
-			chk += xmap[src[i] - '0'] * cmap[cmapindex++];
+			for (int j = 0; j < reImgCopy4.cols; j++)
+			{
+				reImgCopy4.at<uchar>(i, j) = 18 * log(1 + (int)reImg.at<uchar>(i, j));
+			}
 		}
-		else
+		cv::normalize(reImgCopy4, reImgCopy4, 0, 255, CV_MINMAX);
+		cv::convertScaleAbs(reImgCopy4, reImgCopy4);
+
+		char* pOut4 = rec.recognize(reImgCopy4);
+		std::cout << "pOut4: " << pOut4 << std::endl;
+		//f << "pOut4: " << pOut4;
+		cv::imshow("reImgCopy4", reImgCopy4);
+
+		//gamma处理
+		cv::Mat reImgCopy5;
+		reImg.copyTo(reImgCopy5);
+		for (int i = 0; i < reImgCopy5.rows; i++)
 		{
-			continue;
+			for (int j = 0; j < reImgCopy5.cols; j++)
+			{
+				reImgCopy5.at<uchar>(i, j) = saturate_cast<uchar>(pow((float)reImgCopy5.at<uchar>(i, j) / 255, 5.0)*255.0);
+			}
 		}
-		cmapindex = cmapindex % 3;
+		cv::normalize(reImgCopy5, reImgCopy5, 0, 255, CV_MINMAX);
+		cv::convertScaleAbs(reImgCopy5, reImgCopy5);
+
+		char* pOut5 = rec.recognize(reImgCopy5);
+		std::cout << "pOut5: " << pOut5 << std::endl;
+		//f << "pOut5: " << pOut5;
+		cv::imshow("reImgCopy5", reImgCopy5);
+
+		//add
+		cv::Mat reImgCopy6 = (reImgCopy3 + reImgCopy4)/2;
+
+		char* pOut6 = rec.recognize(reImgCopy6);
+		std::cout << "pOut6: " << pOut6 << std::endl;
+		//f << "pOut6: " << pOut6 << std::endl;
+		cv::imshow("reImgCopy6", reImgCopy6);
+
+
+		//分区域作处理
+		cv::Mat reImgCopy7;
+		reImg.copyTo(reImgCopy7);
+		cv::Rect rect7_1(0, 0, reImgCopy7.cols / 2, reImgCopy7.rows);
+		cv::Rect rect7_2(reImgCopy7.cols / 4 - 1, 0, reImgCopy7.cols / 2, reImgCopy7.rows);
+		cv::Rect rect7_3(reImgCopy7.cols / 2 - 1, 0, reImgCopy7.cols / 2, reImgCopy7.rows);
+		cv::Mat kernel1 = (cv::Mat_<float>(3, 3) << 0, -1, 0, 0, 5, 0, 0, -1, 0);
+		cv::filter2D(reImg(rect7_1), reImgCopy7(rect7_1), CV_8UC1, kernel1);
+		cv::imshow("reImgCopy7_1", reImgCopy7);
+		char* pOut7_1 = rec.recognize(reImgCopy7);
+		std::cout << "pOut7_1: " << pOut7_1 << std::endl;
+		//cv::filter2D(reImg(rect7_2), reImgCopy7(rect7_2), CV_8UC1, kernel1);
+		//cv::imshow("reImgCopy7_2", reImgCopy7);
+		cv::filter2D(reImg(rect7_3), reImgCopy7(rect7_3), CV_8UC1, kernel1);
+		cv::imshow("reImgCopy7_3", reImgCopy7);
+		char* pOut7_2 = rec.recognize(reImgCopy7);
+		std::cout << "pOut7_2: " << pOut7_2 << std::endl;
+
+		cv::waitKey(0);
 	}
 
-	return (byte)(chk % 10 + '0');
-}
-
-///护照冗余校验
-bool PassPortCheckCRC(const char *s)
-{
-	int len = strlen(s);
-	if (len<28)return false;
-	if (*s != 'E')return false;
-	/*char subs[29] = { '\0' };
-	strncpy(subs, s, 28);
-
-	//---第一串校验  护照号
-	//E006621742CHN0811220H1706192MNPEHPKGHILLA076
-
-	//E006621742
-	char Bu[20] = {'\0'};
-	strncpy(Bu, subs, 12);
-	byte bCrc = Passport_checkSumSubString((byte *)Bu, 9);
-	if (bCrc != Bu[9])return  false;
-	//0811220
-	memset(Bu, '\0', 20);
-	strncpy(Bu, subs+13, 7);
-	bCrc = Passport_checkSumSubString((byte *)Bu, 6);
-	if (bCrc != Bu[6])return  false;
-	//1706192
-	memset(Bu, '\0', 20);
-	strncpy(Bu, subs + 21, 7);
-	bCrc = Passport_checkSumSubString((byte *)Bu, 6);
-	if (bCrc != Bu[6])return  false;*/
-
-	return true;
-}
-
-///名字校验
-bool NameCheck(const char *s)
-{
-	int len = strlen(s);
-	if (len < 5)
-		return false;
-	if (*s != 'P')
-		return false;
-	/*const char *p = strchr(s, '<');
-	if (p == 0)
-	return false;
-	bool isAlpa = true;
-	int prenum = p - s;
-	for (int i = 0; i < prenum; i++) {
-	if (!isalpha(*(s + i))) {
-	isAlpa = false;
-	break;
-	}
-	}
-	if (!isAlpa)
-	return false;
-	if (*(p + 1) != '<' || !isalpha(*(p + 2)))
-	return false;
-	if (*(s + len - 3) != '<')
-	return false;
-	*/
-	return true;
-}
-
-///港澳通行证
-bool GangaoPass(const char *s)
-{
-	//CSC624902070220802807120406
-	int len = strlen(s);
-	if (len<27)return false;
-	//if(s.contains("CS")) return true;
-	char ss[4] = { '\0' };
-	strncpy(ss, s, 2);
-	if (!(isalpha(ss[0]) && isalnum(ss[1])))
-		return false;
-
-	char Bu[20] = { '\0' };
-	strncpy(Bu, s, 10);
-	byte bCrc = Passport_checkSumSubString((byte *)Bu, 9);
-	if (bCrc != Bu[9])
-		return  false;
-	// C62490207
-	int n = 2;
-	n += 10;
-
-	memset(Bu, '\0', 20);
-	strncpy(Bu, s + 12, 7);
-	bCrc = Passport_checkSumSubString((byte *)Bu, 6);
-	if (bCrc != Bu[6])
-		return  false;
-	memset(Bu, '\0', 20);
-	strncpy(Bu, s + 19, 7);
-	bCrc = Passport_checkSumSubString((byte *)Bu, 6);
-	if (bCrc != Bu[6])
-		return  false;
-	return true;
-}
-
-void trim(std::string &s)
-{
-	int index = 0;
-	if (!s.empty()) {
-		while ((index = s.find(' ', index)) != std::string::npos) {
-			s.erase(index, 1);
-		}
-	}
-}
-
-int main()
-{
-
-	PassImageProc preProc;
-	cv::Mat image;
-	std::vector<cv::Mat> procImgs;
-
-	clock_t total_start, total_end;
-	clock_t detect_start, detect_end;
-
-#ifdef CAM
-	VideoCapture cap(0);
-
-	cap.set(CV_CAP_PROP_FRAME_WIDTH, 1600);
-	cap.set(CV_CAP_PROP_FRAME_HEIGHT, 1200);
-
-	if (!cap.isOpened()) {
-		std::cout << "read camera error!" << std::endl;
-		system("pause");
-		return -1;
-	}
-	while (true) {
-		total_start = clock();
-
-		clock_t readImg_start, readImg_end;
-		readImg_start = clock();
-
-		cap.read(image);
-
-		readImg_end = clock();
-		std::cout << "Read image time: " << readImg_end - readImg_start << "ms" << std::endl;
-
-		if (image.empty())
-		{
-			std::cout << "Frame get failed!" << std::endl;
-			continue;
-		}
-
-		//Mat *pDstImg = new Mat(frame.rows, frame.cols, frame.type());
-		//frame.copyTo(*pDstImg);
-		//Mat srcFrame = *((Mat *)pDstImg);
-
-		detect_start = clock();
-
-		preProc.imgProc(image, Size(28, 8));
-		procImgs = preProc.getResultImageArray();
-
-		detect_end = clock();
-		std::cout << "   Detect time: " << detect_end - detect_start << "ms" << std::endl;
-				
-		imshow("frame", image);
-		char c = waitKey(1);
-		if (c == 27)
-			break;
-
-		total_end = clock();
-		std::cout << "    Total time: " << total_end - total_start << "ms" << std::endl;
-	}
-	cap.release();	
-	destroyAllWindows();
-#endif // CAM
-	
-#ifdef PIC
-	string imageList = "pics_new.txt";
-	//string imageList = "pics_pp.txt";
-	//string imageList = "pics.txt";
-	std::ifstream finImage(imageList, std::ios::in);
-	string line;
-
-	while (getline(finImage, line))
-	{
-		std::cout << line << std::endl;
-		image = imread(line);
-		preProc.imgProc(image, Size(28, 8));
-		procImgs = preProc.getResultImageArray();
-		char c = waitKey(0) & 0xFF;
-		if (c == 'q')
-			break;
-	}
-#endif // PIC
-
-	//system("pause");
 	return 0;
 }
